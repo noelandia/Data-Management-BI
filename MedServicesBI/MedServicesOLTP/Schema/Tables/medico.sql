58afab230d91e9faed9e86c495c4a164bb301c43
roberto olguin <rcolguinl@soe.uagrm.edu.bo>
2025-02-25 18:31:22 -0400

    add cama entity and medico entity

---
+++ MedServicesBI/MedServicesOLTP/Schema/Tables/medico.sql
@@ -0,0 +1,12 @@
+﻿SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [servicios].[medico](
+	[codMedico]  [int] IDENTITY(1,1) NOT NULL,
+	[dni] [int] NOT NULL,
+	[apPaterno] [varchar](200) NULL,
+	[apMaterno] [varchar](200) NULL,
+	[nombres] [varchar](200) NULL,
+	[fechaNacimiento] [date],
+	PRIMARY KEY(codMedico)
+)
+GO
