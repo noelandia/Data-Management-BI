a8ad033fc52b0b78cda487dbb62df13d08a2a7fe
Noel Andia Flores <noel.andiaf@gmail.com>
2025-02-26 01:58:23 -0400

    Se agregó la carpeta Scripts y los primeros script de ejecución post implementación.

---
+++ MedServicesBI/MedServicesOLTP/Scripts/medico.data.sql
@@ -0,0 +1,12 @@
+﻿﻿PRINT 'Populating medico table';
+SET NOCOUNT ON;
+
+SET IDENTITY_INSERT servicios.medico ON;
+
+INSERT INTO servicios.medico(dni,apPaterno,apMaterno,nombres,fechaNacimiento) VALUES ('6401','Perez','Perez','Juan','1980-01-01')
+INSERT INTO servicios.medico(dni,apPaterno,apMaterno,nombres,fechaNacimiento) VALUES ('6402','Bolivar','Palacios','Simon','1985-02-02')
+INSERT INTO servicios.medico(dni,apPaterno,apMaterno,nombres,fechaNacimiento) VALUES ('6403','Fernandez','Taboada','Maria','1990-03-03')
+INSERT INTO servicios.medico(dni,apPaterno,apMaterno,nombres,fechaNacimiento) VALUES ('6404','Revilla','Lenoci','Angel David','1995-04-04')
+INSERT INTO servicios.medico(dni,apPaterno,apMaterno,nombres,fechaNacimiento) VALUES ('6405','Domingo','Murillo','Pedro','2000-05-05')
+
+SET IDENTITY_INSERT servicios.medico OFF;
