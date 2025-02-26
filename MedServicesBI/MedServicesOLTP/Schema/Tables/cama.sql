f813610eee56d8e9805afc2cd90411f23935d091
roberto olguin <rcolguinl@soe.uagrm.edu.bo>
2025-02-25 23:28:20 -0400

    update cama table and medico table

--- MedServicesBI/MedServicesOLTP/Schema/Tables/cama.sql
+++ MedServicesBI/MedServicesOLTP/Schema/Tables/cama.sql
@@ -4,8 +4,8 @@ CREATE TABLE [servicios].[cama](
 	[codCama] [int] NOT NULL,
 	[codHospital] [int] NOT NULL,
 	[idServicio] [int] NOT NULL,
-	[estado] [bit] DEFAULT 1,
-	[nroHabitacion] [int] DEFAULT 0,
+	[nroHabitacion] [varchar](6),
+	[estado] [varchar](20),
 	CONSTRAINT [PK_Cama] PRIMARY KEY(codCama, codHospital, idServicio),
 	CONSTRAINT [FK_Hospital_Servicio_hosp] FOREIGN KEY (codHospital) REFERENCES [servicios].[hospital_servicio](codHospital),
 	CONSTRAINT [FK_Hospital_Servicio_serv] FOREIGN KEY (idServicio) REFERENCES [servicios].[hospital_servicio](idServicio)
