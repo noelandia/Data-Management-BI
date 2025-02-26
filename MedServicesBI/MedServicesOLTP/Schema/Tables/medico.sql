0602843ffa1513f31b73586c2acf5bd1d1bb0259
roberto olguin <rcolguinl@soe.uagrm.edu.bo>
2025-02-25 23:06:16 -0400

    update cama table and medico table

--- MedServicesBI/MedServicesOLTP/Schema/Tables/medico.sql
+++ MedServicesBI/MedServicesOLTP/Schema/Tables/medico.sql
@@ -7,6 +7,16 @@ CREATE TABLE [servicios].[medico](
 	[apMaterno] [varchar](200) NULL,
 	[nombres] [varchar](200) NULL,
 	[fechaNacimiento] [date],
+	CONSTRAINT AK_dni UNIQUE (dni),
 	CONSTRAINT [PK_Medico] PRIMARY KEY (codMedico)
 )
+GO
+
+GO
+INSERT INTO servicios.medico VALUES
+(1,6401,'Perez','Perez','Juan','1980-01-01'),
+(2,6402,'Bolivar','Palacios','Simon','1985-02-02'),
+(3,6403,'Fernandez','Taboada','Maria','1990-03-03'),
+(4,6404,'Revilla','Lenoci','Angel David','1995-04-04'),
+(5,6405,'Domingo','Murillo','Pedro','2000-05-05')
 GO
