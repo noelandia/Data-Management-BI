4889abbd13412d14c65419ad4b1e042dc50632a2
Ana Luisa <Ana Luisa@ANITA>
2025-03-02 01:59:00 -0400

    He creado la tabla Hospital

---
+++ MedServicesBI/MedServicesDW/Schema/Tables/dbo/dimhospital.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[dimhospital]
+(
+    [codhospSK] [int] IDENTITY(1,1) NOT NULL,
+	[codhospital] [int] NOT NULL,
+	[nombre] [varchar](50) NOT NULL,
+	[ciudad] [varchar](20) NOT NULL,
+	[telefono] [int],
+	[coddirector] [int]  NULL,
+	[fechainidir] [date] NULL,
+	[fechafindir] [date] NULL,
+	[rowversion] [timestamp] NOT NULL,
+CONSTRAINT [PK_dimhospital] PRIMARY KEY  CLUSTERED 
+(
+	[codhospSK] ASC
+
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
