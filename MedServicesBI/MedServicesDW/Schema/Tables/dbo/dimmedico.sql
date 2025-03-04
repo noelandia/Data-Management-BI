ef572d68ddfc1a833e88af2681c4f77f2732e191
roberto olguin <rcolguinl@soe.uagrm.edu.bo>
2025-03-03 23:17:14 -0400

    update columns on table dimmedico

--- MedServicesBI/MedServicesDW/Schema/Tables/dbo/dimmedico.sql
+++ MedServicesBI/MedServicesDW/Schema/Tables/dbo/dimmedico.sql
@@ -1,12 +1,11 @@
 ﻿CREATE TABLE [dbo].[dimmedico](
-    [codMedico] [int] NOT NULL,
+    [codMedico] [int] IDENTITY(1,1) NOT NULL,
 	[dni] [varchar](20) NOT NULL,
 	[apPaterno] [varchar](50),
 	[apMaterno] [varchar](50),
 	[nombres] [varchar](50) NOT NULL,
 	[fechaNacimiento] [date],
 	[codDirector] [int] NOT NULL,
-	CONSTRAINT AK_dni UNIQUE (dni),
 	CONSTRAINT [PK_Medico] PRIMARY KEY (codMedico)
 )
 GO
