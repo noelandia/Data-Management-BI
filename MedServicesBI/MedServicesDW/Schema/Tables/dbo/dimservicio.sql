1dddae134f6a290151f464033f7bb4fcace08cdd
usuario-github <poderconocer@gmail.com>
2025-03-05 10:11:09 -0400

    Creacion de tabla servicio (dimension) para datawarehouse esquema estrella.

---
+++ MedServicesBI/MedServicesDW/Schema/Tables/dbo/dimservicio.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[dimservicio]
+(
+	[idServicioSK] [int] IDENTITY(1,1) NOT NULL,
+	[idServicio] [int] NOT NULL,
+	[nombre] NVARCHAR(200) NOT NULL,
+	[acronimo] NVARCHAR(50),
+	[comentario] NVARCHAR(1000),
+	CONSTRAINT [pk_dimservicio] PRIMARY KEY CLUSTERED ([idServicioSK] ASC)
+)
