cdb7ac9cd713c1fa988229285267312459cd0e21
Noel Andia Flores <noel.andiaf@gmail.com>
2025-03-05 17:33:28 -0400

    Igualación de formatos.

--- MedServicesBI/MedServicesDW/Schema/Tables/dbo/dimhospital.sql
+++ MedServicesBI/MedServicesDW/Schema/Tables/dbo/dimhospital.sql
@@ -8,10 +8,6 @@
 	[coddirector] [int]  NULL,
 	[fechainidir] [date] NULL,
 	[fechafindir] [date] NULL,
-CONSTRAINT [PK_DimHospital] PRIMARY KEY  CLUSTERED 
-(
-	[codHospitalSK] ASC
-
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+	CONSTRAINT [PK_DimHospital] PRIMARY KEY ([codHospitalSK])
+)
 GO
