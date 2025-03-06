e0091d13d2cc4d01ffed0f8ff4cdb92ba08d8a6b
Noel Andia Flores <noel.andiaf@gmail.com>
2025-03-06 17:30:24 -0400

    Se agregó el esquema staging, las tablas PackageConfig, staging.paciente y los procedimientos almacenados correspondientes para el Data Warehouse.

---
+++ MedServicesBI/MedServicesDW/Schema/Programmability/Stored Procedures/DW_MergeDimPaciente.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimPaciente]
+AS
+BEGIN
+	UPDATE dp
+	   SET dni = sp.dni,
+		   apPaterno = sp.apPaterno,
+		   apMaterno = sp.apMaterno,
+		   nombres = sp.nombres,
+		   fecNacimiento = sp.fecNacimiento,
+		   nroSeguro = sp.nroSeguro,
+		   nroCelular = sp.nroCelular
+	  FROM dbo.DimPaciente dp
+	  INNER JOIN staging.paciente sp ON dp.codHistSK = sp.codHistSK;
+END
+GO
