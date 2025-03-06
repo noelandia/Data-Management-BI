6c47933e78382829c4b5e3707370ea776da18e12
Ana Luisa <Ana Luisa@ANITA>
2025-03-06 17:53:16 -0400

    Se ha creado los procedimientos almacenados para la Base de Datos, Tabla Hospital y Tabla HospitaServicio del OLTP

---
+++ MedServicesBI/MedServicesOLTP/Schema/Programmability/hospital.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE PROCEDURE [dbo].[GethospitalChangesByRowVersion]
+(
+   @startRow BIGINT,
+   @endRow  BIGINT 
+)
+AS
+BEGIN
+	SELECT codHospital,
+		   nombre,
+		   ciudad,
+		   telefono
+	 FROM servicios.hospital
+	 WHERE rowversion > CONVERT(ROWVERSION, @startRow) 
+	   AND rowversion <= CONVERT(ROWVERSION,@endRow)
+END
+GO
