85ef95b5557a6ecc6a316aff693c2d3f5c61af76
Noel Andia Flores <noel.andiaf@gmail.com>
2025-03-06 02:45:14 -0400

    Se han creado los procedimientos almacenados para la base de datos, tabla director y tabla visita_medica del proyecto OLTP.

---
+++ MedServicesBI/MedServicesOLTP/Schema/Programmability/Stored Procedures/GetDirectorChangesByRowVersion.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetDirectorChangesByRowVersion]
+(
+   @startRow BIGINT,
+   @endRow  BIGINT 
+)
+AS
+BEGIN
+	SELECT codDirector,
+		   codHospital,
+		   fecInicio,
+		   fecFin,
+		   codMedico
+	  FROM servicios.director
+	 WHERE rowversion > CONVERT(ROWVERSION, @startRow) 
+	   AND rowversion <= CONVERT(ROWVERSION,@endRow)
+END
+GO
