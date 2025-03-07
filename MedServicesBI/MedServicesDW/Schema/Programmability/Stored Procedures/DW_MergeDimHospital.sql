df8cc4c5f67924d6868295f22acdb96805cde5a7
Ana Luisa <Ana Luisa@ANITA>
2025-03-07 01:33:26 -0400

    Se han creado los procedimientos almacenados para la Base de Datos, de la tabla Hospital, Director de OLTP y DW

---
+++ MedServicesBI/MedServicesDW/Schema/Programmability/Stored Procedures/DW_MergeDimHospital.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimHospital]
+AS
+BEGIN
+
+	UPDATE dc
+	SET [codHospital]  = sc.[codHospital]
+	   ,[nombre]     = sc.[nombre]
+	   ,[ciudad]     = sc.[ciudad]
+	   ,[telefono]    = sc.[telefono]
+	   ,[coddirector] = sc.[coddirector]
+	   ,[fechainidir]     = sc.[fechainidir]
+	   ,[fechafindir]   = sc.[fechafindir]
+ 	   FROM [dbo].[DimHospital]        dc
+	   INNER JOIN [staging].[director] sc ON (dc.[codHospitalSK]=sc.[codHospitalSK])
+END
+GO
+
