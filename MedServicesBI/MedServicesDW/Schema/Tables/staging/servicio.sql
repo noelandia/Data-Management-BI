40d84033eb8d1a8996e5ce6baeb1c8f86f41878e
usuario-github <poderconocer@gmail.com>
2025-03-07 18:56:31 -0400

    Actualizacion de tabla staging, procedimiento almacenado para tabla DimServicio de la base de datos DW.

---
+++ MedServicesBI/MedServicesDW/Schema/Tables/staging/servicio.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [staging].[servicio]
+(
+	idServicioSK int NOT NULL,
+	idServicio int NOT NULL,
+	nombre NVARCHAR(200) NOT NULL,
+	acronimo NVARCHAR(50),
+	comentario NVARCHAR(1000)
+)
+GO
