95b03d2bf9de98d2f202e558639e23c8d417d48c
usuario-github <poderconocer@gmail.com>
2025-03-06 20:32:07 -0400

    Creacion de procedimientos almacenados ChangesByRowVersion para tablas paciente, medico_servicio base de datos OLTP.

---
+++ MedServicesBI/MedServicesOLTP/Schema/Programmability/Stored Procedures/GetMedicoServicioChangesByRowVersion.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetMedicoServicioChangesByRowVersion]
+(
+   @startRow BIGINT,
+   @endRow  BIGINT 
+)
+AS
+BEGIN
+	SELECT codMedico,
+			idServicio
+	  FROM servicios.medico_servicio
+	 WHERE rowversion > CONVERT(ROWVERSION, @startRow) 
+	   AND rowversion <= CONVERT(ROWVERSION,@endRow)
+END
+GO
